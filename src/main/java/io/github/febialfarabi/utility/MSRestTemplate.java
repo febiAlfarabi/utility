06c8f549bcb13490b5355f4e29950e9d3aa0f4a0
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-31 01:31:03 +0700

    -

--- src/main/java/io/github/febialfarabi/utility/MSRestTemplate.java
+++ src/main/java/io/github/febialfarabi/utility/MSRestTemplate.java
@@ -20,97 +20,97 @@ public class MSRestTemplate extends RestTemplate {
 
     @Override
     public <T> ResponseEntity<T> exchange(RequestEntity<?> requestEntity, Class<T> responseType) throws RestClientException {
-        logger.log(Level.INFO, "HIT :: {} ", requestEntity.getUrl().toString());
+        logger.log(Level.INFO, "HIT :: "+requestEntity.getUrl().toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(requestEntity, responseType);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(RequestEntity<?> requestEntity, ParameterizedTypeReference<T> responseType) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", requestEntity.getUrl().toString());
+        logger.log(Level.INFO, "HIT :: "+requestEntity.getUrl().toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(requestEntity, responseType);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(URI url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(url, method, requestEntity, responseType);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(url, method, requestEntity, responseType, uriVariables);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType, Map<String, ?> uriVariables) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(url, method, requestEntity, responseType, uriVariables);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(URI url, HttpMethod method, HttpEntity<?> requestEntity, ParameterizedTypeReference<T> responseType) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity =  super.exchange(url, method, requestEntity, responseType);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, ParameterizedTypeReference<T> responseType, Object... uriVariables) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity = super.exchange(url, method, requestEntity, responseType, uriVariables);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
     }
 
     @Override
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, ParameterizedTypeReference<T> responseType, Map<String, ?> uriVariables) throws RestClientException {
-        logger.log(Level.INFO,"HIT :: {} ", url.toString());
+        logger.log(Level.INFO, "HIT :: "+url.toString());
         requestEntity.getHeaders().entrySet().forEach(stringListEntry -> {
-            logger.log(Level.INFO, "HIT Header name : {} #### Header value : {} ", new String[]{stringListEntry.getKey(), stringListEntry.getValue().toString()});
+            logger.log(Level.INFO, "HIT Header name : "+stringListEntry.getKey()+" #### Header value : "+stringListEntry.getValue().toString());
         });
-        logger.log(Level.INFO,"HIT WITH BODY = {} ", gson.toJson(requestEntity.getBody()));
+        logger.log(Level.INFO,"HIT WITH BODY = "+gson.toJson(requestEntity.getBody()));
         ResponseEntity responseEntity = super.exchange(url, method, requestEntity, responseType, uriVariables);
-        logger.log(Level.INFO,"RESPONSE = {}", responseEntity.toString());
+        logger.log(Level.INFO,"RESPONSE = "+ responseEntity.toString());
         return responseEntity;
 
     }
