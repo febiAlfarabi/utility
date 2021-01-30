db614af239d2015e3475e447d725a62ed222e4d7
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-31 04:41:43 +0700

    -

--- src/main/java/io/github/febialfarabi/utility/MSRestTemplate.java
+++ src/main/java/io/github/febialfarabi/utility/MSRestTemplate.java
@@ -6,10 +6,28 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.http.converter.feed.AtomFeedHttpMessageConverter;
+import org.springframework.http.converter.feed.RssChannelHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
+import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.smile.MappingJackson2SmileHttpMessageConverter;
+import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +35,15 @@ import java.util.logging.Logger;
 public class MSRestTemplate extends RestTemplate {
     java.util.logging.Logger logger = Logger.getLogger(MSRestTemplate.class.getName());
     Gson gson = new Gson();
+
+    public MSRestTemplate() {
+        super();
+    }
+
+    public MSRestTemplate(ClientHttpRequestFactory requestFactory) {
+        super(requestFactory);
+    }
+
 
     @Override
     public <T> ResponseEntity<T> exchange(RequestEntity<?> requestEntity, Class<T> responseType) throws RestClientException {
